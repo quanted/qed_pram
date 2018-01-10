2ae05d1ae76c9d1276d19b6db7d19ad8014e0406
Tom Purucker <purucker.tom@gmail.com>
2018-01-10 15:07:50 -0500

    django 2.0 path routing

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
 
+# Django 2.0 url import
+from django.urls import path
+
 #regular expressions
 # the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
@@ -8,8 +11,10 @@ from django.conf.urls import include, url
 print('qed.urls')
 #appends to the list of url patterns to check against
 urlpatterns = [
-	url(r'^', include('splash_app.urls')),
-    #url(r'^hwbi/', include('hwbi_app.urls')),
-    url(r'^ubertool', include('ubertool_app.urls')),
-    #url(r'^cts/', include('cts_app.urls')),
+    ##django 1.X
+	#url(r'^', include('splash_app.urls')),
+    #url(r'^ubertool', include('ubertool_app.urls')),
+    #django 2.X
+    path('', include('splash_app.urls')),
+    path('ubertool', include('ubertool_app.urls')),
 ]
